b5356c9b1c19e4307e05c16291df5d38452d41c9
Shigeru Yamabayashi <anma555@me.com>
2024-10-09 07:13:29 +0900

    掛け算の機能を追加

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 def main():
-    print(add(1, 2))
-    print(sub(2, 1))
+    print("{} + {} = {}".format(1, 2, add(1, 2)))
+    print("{} - {} = {}".format(2, 1, sub(2, 1)))
+    print("{} * {} = {}".format(2, 2, mul(2, 2)))
     
 if __name__ == '__main__':
     main()
