aa9638794b9c9f846f1592b0097c8591cd7e824a
Shigeru Yamabayashi <anma555@me.com>
2024-10-09 07:19:33 +0900

    結果表示を変更

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def sub(a, b):
     return a - b
 
 def main():
-    print(add(1, 2))
-    print(sub(2, 1))
+    print("{}と{}を足すと{}です。".format(1, 2, add(1, 2)))
+    print("{}から{}を引くと{}です。".foramt(2, 1, sub(2, 1)))
     
 if __name__ == '__main__':
     main()
